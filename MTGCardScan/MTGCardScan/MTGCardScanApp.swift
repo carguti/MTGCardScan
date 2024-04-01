d610c342f1b2f275e5e5aeda240bd67b62d2cdd8
Carlos Gutiérrez Casado <carlosgutierrezcasado.1991@gmail.com>
2024-04-01 20:02:36 +0200

    Project created

---
+++ MTGCardScan/MTGCardScan/MTGCardScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGCardScanApp.swift
+//  MTGCardScan
+//
+//  Created by Carlos Gutiérrez Casado on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MTGCardScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
