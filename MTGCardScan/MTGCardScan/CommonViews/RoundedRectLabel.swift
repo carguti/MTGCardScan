e597612e7e1770f8426127c3b90104c7286f9243
Carlos Gutiérrez Casado <carlosgutierrezcasado.1991@gmail.com>
2024-04-23 18:00:02 +0200

    First approach to ScanCardView and functionality Changed API call to get card data by the card name

---
+++ MTGCardScan/MTGCardScan/CommonViews/RoundedRectLabel.swift
@@ -0,0 +1,8 @@
+//
+//  RoundedRectLabel.swift
+//  MTGCardScan
+//
+//  Created by Carlos Gutiérrez Casado on 16/4/24.
+//
+
+import Foundation
---
+++ MTGCardScan/MTGCardScan/Screens/ScanCard/ScanCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanCardView.swift
+//  MTGCardScan
+//
+//  Created by Carlos Gutiérrez Casado on 15/4/24.
+//
+
+import SwiftUI
+
+struct ScanCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanCardView()
+}
